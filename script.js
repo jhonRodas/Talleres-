9f8116c1110f7a37c6d1fc24a4bbad1b54cf54ca
Jhon Henry rodas <jhonrodas0370@gmail.com>
2024-10-30 19:26:58 -0500

    se agreo atributo carrera

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+
+//variable string//
 let mydogs = "KIARA,MAYA"
 console.log (mydogs)
+
+
+//variable booleano//
+let esDia = false;
+let esNoche = false;
+
+if (esDia) {
+    console.log("Es de Dia");
+    }
+if (esNoche) {
+    console.log ("Es de noche");
+    } 
+    else {
+        console.log("No es de noche")
+    }
+
+
+
+
+
+
+
+    console.log ("kiaraesPequeña")
