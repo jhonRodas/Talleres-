8f27a07d9d2e7eccc64c5a200c1e16d215470b59
Jhon Henry rodas <jhonrodas0370@gmail.com>
2024-10-31 23:36:43 -0500

    Se agregaron entero,decimales,arreglos y Boleanos

--- script.js
+++ script.js
@@ -18,10 +18,28 @@ if (esNoche) {
         console.log("No es de noche")
     }
 
-
-
-
-
-
-
     console.log ("kiaraesPequeña")
+
+    // Numeros enteros
+
+let numero1 = 50;
+let numero2 = 60;
+
+
+let suma = numero1 + numero2;
+console.log("La suma es:", suma);
+
+
+// Decimales
+
+let numero3 = 10.4;
+let numero4 = 6.6;
+
+let division = numero3 / numero4;
+console.log("La división es:", division)
+
+const FRUIST = ["platano", "manzana", "pera", "naranja", "fresa",]
+
+FRUIST.forEach((fruit, index) => {
+    console.log(`fruta ${fruit} ${index}`)
+})
