e48af2ddb3a58242a76e4aa90599396abdb0182a
Jhon Henry rodas <jhonrodas0370@gmail.com>
2024-11-06 23:58:37 -0500

    TallerEmpleado

---
+++ TallerEmpleado.js
@@ -0,0 +1,25 @@
+class Empleado {
+    constructor(nombre, salario, puesto) {
+        this.nombre = nombre;
+        this.salario = salario;
+        this.puesto = puesto;
+    }
+
+    incrementarSalario(incremento) {
+        this.salario += incremento;
+    }
+
+    mostrarDatos() {
+        return `Nombre: ${this.nombre} Salario: ${this.salario} Puesto: ${this.puesto}`;
+    }
+}
+
+let empleado = new Empleado('Juan', 1000000, 'Gerente');
+
+console.log(empleado.mostrarDatos());
+
+empleado.incrementarSalario(500000);
+
+console.log(empleado.mostrarDatos());
+
+
